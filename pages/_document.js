04862cad073806b4f21b7aec068580743bf56484
Almer Tampus <almerflorestampus@gmail.com>
2023-08-22 23:11:12 +0800

    Add testing script from freeCodeCamp

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
         <NextScript />
+        <script
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js'
+          defer
+        ></script>
       </body>
     </Html>
-  )
+  );
 }
