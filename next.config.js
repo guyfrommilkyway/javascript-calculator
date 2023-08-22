3221d0fda9c5f50f0d404e4b4671307c53a1a66f
Almer Tampus <almerflorestampus@gmail.com>
2023-08-22 23:09:27 +0800

    Add page extensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ['page.jsx', 'page.js', 'jsx', 'js'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
